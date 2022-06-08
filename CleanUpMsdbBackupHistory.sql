8f999381d521078bf58b7d3e8d1746d17a70463d
Mark Allison <Mark.Allison@hee.nhs.uk>
2022-06-08 11:12:51 +0100

    remove 3 part db references

--- CleanUpMsdbBackupHistory.sql
+++ CleanUpMsdbBackupHistory.sql
@@ -11,28 +11,30 @@ DECLARE @CleanupDate DATETIME
 
 -- get the recovery model of msdb
 DECLARE @recovery_model VARCHAR (20) 
-SELECT @recovery_model = recovery_model_desc FROM sys.databases where name = 'msdb'
+SELECT @recovery_model = recovery_model_desc 
+FROM sys.databases 
+WHERE [name] = DB_NAME()
 
 -- set the cleanup date
 SET @CleanupDate = DATEADD(dd, @DaysToKeep * -1, GETDATE())
-PRINT 'Keeping history up to: ' + CAST(@CleanupDate AS varchar(30))
+PRINT 'Keeping history up to: ' + CAST(@CleanupDate AS VARCHAR(30))
 
 -- get the oldest date in the history
 DECLARE @min_date DATETIME
 SELECT @min_date = min(backup_finish_date)
-from msdb.dbo.backupset
+FROM dbo.backupset
 
-PRINT 'Oldest date in history: ' + CAST(@min_date AS varchar(30))
+PRINT 'Oldest date in history: ' + CAST(@min_date AS VARCHAR(30))
 
 -- while the oldest date in the history is older than the cleanup date
 WHILE (@min_date < @CleanupDate)
 BEGIN   
     -- set the chunk date to be oldest date + chunk days
     SET @ChunkDate = DATEADD(dd, @chunk_days, @min_date)
-    PRINT 'Removing history older than ' + CAST( @ChunkDate AS varchar(30))
+    PRINT 'Removing history older than ' + CAST( @ChunkDate AS VARCHAR(30))
 
     -- delete the data up to the last chunk date
-    EXECUTE msdb.dbo.sp_delete_backuphistory @oldest_date = @ChunkDate
+    EXECUTE dbo.sp_delete_backuphistory @oldest_date = @ChunkDate
     
     -- checkpoint the database so log can be truncated
     IF (@recovery_model = 'SIMPLE')
@@ -43,10 +45,10 @@ BEGIN
 
     -- get the oldest date in the history
     SELECT @min_date = min(backup_finish_date)
-    from msdb.dbo.backupset
-    PRINT 'Oldest date in history: ' + CAST(@min_date AS varchar(30))
+    FROM dbo.backupset
+    PRINT 'Oldest date in history: ' + CAST(@min_date AS VARCHAR(30))
 END
 
 -- final cleanup
-PRINT 'Running final cleanup older than date: ' + CAST(@CleanupDate AS varchar(30))
-EXECUTE msdb.dbo.sp_delete_backuphistory @oldest_date = @CleanupDate
+PRINT 'Running final cleanup older than date: ' + CAST(@CleanupDate AS VARCHAR(30))
+EXECUTE dbo.sp_delete_backuphistory @oldest_date = @CleanupDate
