1218507671f8d0dfb56db2a9059f0b58c82af61d
Mark Allison <mark.allison@hee.nhs.uk>
2023-03-21 17:17:52 +0000

    Initial infrastructure draft

---
+++ adf-ir-sqlmi-demo/Deploy.ps1
@@ -0,0 +1,183 @@
+[cmdletbinding()]
+param (
+    [Parameter (Mandatory = $false)]  [string]    $Location = "uksouth",
+    [Parameter (Mandatory = $false)]  [string]    $Subscription = "HEE DATA WAREHOUSE"
+)
+function ConvertToHashTable {
+    param (
+        $rules
+    )
+
+    $inboundPosition = 100
+    $outboundPosition = 100
+    $index = 0
+
+    $result = [Object[]]::new($rules.Length)
+ 
+
+    foreach ($rule in $rules) {
+        
+        $ruleHash = @{}
+
+        $ruleHash["name"] = "Microsoft.Sql-managedInstances_UseOnly_" + $rule.name
+
+        $propertiesHash = @{};
+
+        foreach ($property in $rule.properties.PSObject.Properties) {
+            $name = $property.Name
+            $value = $property.Value
+            if ($name -eq "sourceAddressPrefixes" -and $value.Length -eq 1) {
+                $name = "sourceAddressPrefix"
+                $value = $value[0]
+            }
+            if ($name -eq "destinationAddressPrefixes" -and $value.Length -eq 1) {
+                $name = "destinationAddressPrefix"
+                $value = $value[0]
+            }
+            if ($name -eq "sourcePortRanges" -and $value -eq '*') {
+                $name = "sourcePortRange"
+                $value = "*"
+            }
+            if ($name -eq "destinationPortRanges" -and $value -eq '*') {
+                $name = "destinationPortRange"
+                $value = "*"
+            }
+            if ($name -eq "direction" -and $value -eq 'Inbound') {
+                $propertiesHash['priority'] = $inboundPosition++
+    
+            }
+            if ($name -eq "direction" -and $value -eq 'Outbound') {
+                $propertiesHash['priority'] = $outboundPosition++
+            }
+            if (
+                $name -ne 'addedBySystem' -and
+                $name -ne 'provisioningState'
+            ) {
+                $propertiesHash[$name] = $value
+            }
+        }
+
+        $ruleHash["properties"] = $propertiesHash
+
+        $result[$index++] = $ruleHash
+    }
+
+    return $result
+}
+$ErrorActionPreference = 'Stop'
+
+Write-Information "Exporting network policies for SQLMI"
+
+# for new deployments we don't have any network intent policy, so create empty arrays
+"[]" > "$PSScriptRoot\network-intent-policy-rules.json"
+"[]" > "$PSScriptRoot\network-intent-policy-routes.json"        
+
+# Get my public IP
+Write-Verbose "Getting my public IP"
+$ip = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content | ConvertTo-SecureString -AsPlainText -Force
+
+# read the parameters file
+$params = (Get-Content -Path "./parameters/$($location).json" | ConvertFrom-Json).parameters
+
+# check deplying to correct subscription
+$sub = Get-AzContext
+if ($sub.Subscription.Name -ne $Subscription) {
+    throw "$($sub.Subscription.Name) is the wrong subscription."
+}
+
+$ResourceGroup = $params.resourceGroup.value
+
+$securityRules = @()
+$routes = @()
+$apiVersion = "2020-07-01"
+
+if (Get-AzResourceGroup | where { $_.ResourceGroupName -eq $ResourceGroup }) {
+    Write-Information "Getting VNet info for existing resource group: $ResourceGroup"
+    $vnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroup
+    # $vnet.SubnetsText
+
+    if ($null -ne $vnet) {
+        $miSubnetResourceId = ($vnet.Subnets | where { $_.Name -eq 'ManagedInstance' }).Id
+
+        if ($null -ne $miSubnetResourceId) { 
+            Write-Information "Getting subnet"
+            # get the MI subnet
+            $subnet = Get-AzResource -ResourceId $miSubnetResourceId -ApiVersion $apiVersion
+		
+            if ($subnet.Properties.networkIntentPolicies.Length -gt 0) {
+                Write-Information "Getting rules from the network policy"
+                $nipId = $subnet.Properties.networkIntentPolicies[0].id
+                $nip = Get-AzResource -ResourceId $nipId -ApiVersion $apiVersion 
+                $securityRules = $nip.Properties.securityRules
+                $routes = $nip.Properties.routes
+                $securityRules = ConvertToHashTable $securityRules   
+                $securityRules | ConvertTo-Json -Depth 3 > "$PSScriptRoot\network-intent-policy-rules.json"
+
+                $routes = ConvertToHashTable $routes    
+                $routes | ConvertTo-Json -Depth 3 > "$PSScriptRoot\network-intent-policy-routes.json"
+            }
+            else {
+                Write-Information "Network policy not found"
+            }
+        }
+        else {
+            Write-Information "Subnet not found"
+        }
+    }
+    else {
+        Write-Information "VNet not found"
+    }
+}
+else {
+    Write-Information "Resource Group $ResourceGroup not found"
+}
+
+Write-Information "Get secret from key vault if exists"
+$kv = Get-AzKeyVault -ResourceGroupName $ResourceGroup
+
+$myAccount = Get-AzADUser -UserPrincipalName $sub.Account
+
+$keyVaultRoleAssignments = @(
+    @{
+        RoleDefinitionId = (Get-AzRoleDefinition -Name "Key Vault Secrets User").Id
+        PrincipalId      = $myAccount.Id
+    },
+    @{
+        RoleDefinitionId = (Get-AzRoleDefinition -Name "Key Vault Secrets Officer").Id
+        PrincipalId      = $myAccount.Id
+    }
+)
+
+$adfRoleAssignments = @(
+    @{
+        RoleDefinitionId = (Get-AzRoleDefinition -Name "Data Factory Contributor").Id
+        PrincipalId      = $myAccount.Id
+    }
+)
+
+
+# Define the deployment parameters
+$deployParams = @{
+    Name                    = "vm.deploy.$(Get-Date -Format 'yyyyMMdd.HHmmss')"
+    Location                = $Location
+    TemplateFile            = "./main.bicep"
+    TemplateParameterFile   = "./parameters/$Location.json" 
+    allowedIp               = $ip 
+    keyVaultRoleAssignments = $keyVaultRoleAssignments
+    adfRoleAssignments      = $adfRoleAssignments
+}
+
+if ($kv) {
+    Write-Information "Found key vault $($kv.VaultName). Using existing secret."
+    $secret = Get-AzKeyVaultSecret -VaultName $kv.VaultName -Name $params.adminUserName.value -AsPlainText
+    if ($secret) {
+        $deployParams.Add("adminPassword", $secret.SecretValue)
+    }
+    else {
+        Write-Information "Could not find secret $($params.adminUserName.value) in $($kv.VaultName)"
+    }
+    
+}
+
+Write-Information "Deploying infra"
+New-AzDeployment @deployParams
---
+++ adf-ir-sqlmi-demo/main.bicep
@@ -0,0 +1,194 @@
+@description('Azure Data Factory Name')
+param adfName string
+
+@description('A list of Object Ids in AAD to allow to ADF')
+param adfRoleAssignments array
+
+@description('The admin password of the VM')
+@secure()
+param adminPassword string
+
+@description('The admin user of the VM')
+param adminUserName string
+
+@description('Ip address whitelisted to the public endpoint')
+@secure()
+param allowedIp string
+
+@description('Suffix to make deployment names unique')
+param deploymentNameSuffix string = utcNow()
+
+@description('A list of Object Ids in AAD to allow to Key Vault')
+param keyVaultRoleAssignments array
+
+@description('Azure Region')
+param location string = deployment().location
+
+@description('Name of the resource group')
+param resourceGroup string
+
+@description('Managed Instance Network security group name.')
+param sqlmiNetworkSecurityGroupName string
+
+@description('The SQLMI collation')
+param sqlmiCollation string
+
+@description('The license to use for SQLMI')
+param sqlmiLicenseType string
+
+@description('Name of the Managed Instance subnet route table')
+param sqlmiRouteTableName string
+
+@description('Managed Instance Compute type and capacity')
+param sqlmiSkuName string
+
+@description('Storage account type for PIT backups')
+param sqlmiBackupStorageRedundancy string
+
+@description('Initial Storage size for the SQLMI.')
+param sqlmiStorageSizeInGB int
+
+@description('MI subnet address IP range.')
+param sqlmiSubnetAddressRange string
+
+@description('Number of vCores')
+param sqlmiVcores int
+
+@description('vm Network security group name.')
+param vmNetworkSecurityGroupName string
+
+@description('The size of the VM')
+param vmSize string
+
+@description('MI subnet address IP range.')
+param vmSubnetAddressRange string
+
+@description('Virtual network IP address range.')
+param vnetAddressRange string
+
+@description('Name of the subnet for the VM.')
+param vmSubnetName string = 'vm-subnet'
+
+@description('All resources should be tagged :)')
+param tags object
+
+param tenantId string = subscription().tenantId
+
+@description('Vnet name')
+param vnetName string
+
+// create a unique suffix to prevent global name clashes
+var suffix = toLower(uniqueString(subscription().id, rg.id))
+
+// key vault name
+var kvName = 'kv${suffix}'
+// vm name
+var vmName = 'vm${suffix}'
+// sqlmi name
+var sqlmiName = 'sqlmi${suffix}'
+var secrets = [
+  {
+    name: adminUserName
+    value: adminPassword
+  }
+]
+
+// create the resource group
+targetScope = 'subscription'
+
+resource rg 'Microsoft.Resources/resourceGroups@2021-04-01' = {
+  name: resourceGroup
+  location: location
+  tags: tags
+}
+
+// create a keyvault to re-use secrets
+module kv './modules/keyVault.bicep' =  {
+  name: '${kvName}.${deploymentNameSuffix}'
+  params: {
+    keyVaultName: kvName
+    location: location
+    roleAssignments: keyVaultRoleAssignments
+    secrets: secrets
+    tags: tags
+    tenantId: tenantId
+  }
+  scope: rg
+}
+
+//  create vnet
+module vnet './modules/vnet.bicep' = {
+  name: '${vnetName}.${deploymentNameSuffix}'
+  params: {
+    allowedIp: allowedIp
+    location: rg.location
+    sqlmiNetworkSecurityGroupName: sqlmiNetworkSecurityGroupName
+    sqlmiRouteTableName: sqlmiRouteTableName
+    sqlmiSubnetAddressRange: sqlmiSubnetAddressRange
+    tags: tags
+    vmNetworkSecurityGroupName: vmNetworkSecurityGroupName
+    vmSubnetName: vmSubnetName
+    vmSubnetAddressRange: vmSubnetAddressRange
+    vnetAddressRange: vnetAddressRange
+    vnetName: vnetName
+  }
+  scope: rg
+}
+
+// create the vm for hosting IR
+module vm './modules/vm.bicep' = {
+  name: '${vmName}.${deploymentNameSuffix}'
+  params: {
+    adminPassword: adminPassword
+    adminUserName: adminUserName
+    location: location
+    osDiskType: 'Standard_LRS'
+    subnetName: vmSubnetName
+    tags: tags
+    vmName: vmName
+    vmSize: vmSize
+    vnetName: vnetName
+    vnetResourceGroup: resourceGroup
+    pipSku: 'Basic'
+  }
+  scope: rg
+  dependsOn: [
+    vnet
+  ]
+}
+
+// create a SQLMI
+module sqlmi './modules/mi.bicep' = {
+  name: '${sqlmiName}.${deploymentNameSuffix}'
+  params: {
+    adminLogin: adminUserName
+    adminPassword: adminPassword
+    backupStorageRedundancy: sqlmiBackupStorageRedundancy
+    collation: sqlmiCollation
+    licenseType: sqlmiLicenseType
+    location: rg.location    
+    sqlmiName: sqlmiName
+    sqlmiSkuName: sqlmiSkuName
+    sqlmiStorageSizeInGB: sqlmiStorageSizeInGB
+    sqlmiVcores: sqlmiVcores
+    tags: tags
+    vnetName: vnetName
+    vnetResourceGroupName: resourceGroup
+  }
+  scope: rg
+}
+
+module adf './modules/adf.bicep' = {
+  name: '${adfName}.${deploymentNameSuffix}'
+  params: {
+    location: location
+    name: adfName
+    roleAssignments: adfRoleAssignments
+    tags: tags
+  }
+  scope: rg
+}
+
+output vmName string = vmName
+output keyVaultName string = kvName
+output sqlmiName string = sqlmiName
